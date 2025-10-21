82395d5ec3a88b7fde3ed55ebccf6813f81c0daa
soyoonL <diamomo777@naver.com>
2025-10-22 01:47:55 +0900

    [fix] 무기나 아이템을 획득할 때 일어나는 물리충돌 제거

--- Team3_NecroByte/Assets/Scripts/Item.cs
+++ Team3_NecroByte/Assets/Scripts/Item.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Item : MonoBehaviour
@@ -9,8 +10,26 @@ public class Item : MonoBehaviour
     public Type type;
     public int value;
 
+    Rigidbody rigid;
+    SphereCollider sphereCollider;
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody>();
+        sphereCollider = GetComponent<SphereCollider>();
+    }
+
     void Update()
     {
         transform.Rotate(Vector3.up*20*Time.deltaTime);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Floor")
+        {
+            rigid.isKinematic = true;
+            sphereCollider.enabled = false;
+        }
     }
 }
